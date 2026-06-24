9be28025603273edd6d786d66b479aba800570dc
Venuri Perera <venuriperera419@gmail.com>
2026-06-24 13:04:50 +0530

    feat: initialize Spring Boot backend project with Maven wrapper

---
+++ dorm-flow-backend/src/main/java/com/dormflow/dorm_flow_backend/DormFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dormflow.dorm_flow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormFlowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormFlowBackendApplication.class, args);
+	}
+
+}
---
+++ dorm-flow-backend/src/test/java/com/dormflow/dorm_flow_backend/DormFlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dormflow.dorm_flow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormFlowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
